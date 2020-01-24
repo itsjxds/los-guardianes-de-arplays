21b166304108b8e6ba952a17c7af12d86806ad0f
itsjxds <juddy.zombye@gmail.com>
2020-01-24 12:20:26 +0100

    proyecto prueba arlays copiado

---
+++ develop/COPIA ANTES TILEMAP/Los Guardianes de Arlays/Assets/Scripts/Character.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character
+{
+    public int health;
+    public int attack;
+
+    public Character(int health, int attack)
+    {
+        this.attack = attack;
+        this.health = health;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
