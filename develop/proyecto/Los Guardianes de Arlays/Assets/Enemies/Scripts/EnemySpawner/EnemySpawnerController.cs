5a4a047a0124fcf5d2cee2035b9e4f0488c022a8
itsjxds <juddy.zombye@gmail.com>
2020-01-27 12:41:57 +0100

    level01 - muralla y spawn points terminados

---
+++ develop/proyecto/Los Guardianes de Arlays/Assets/Enemies/Scripts/EnemySpawner/EnemySpawnerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerController : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] enemies;
+    public bool spawnerAllowed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("spawnEnemy", 0f, 2.5f);
+    }
+
+    private void spawnEnemy ()
+    {
+        if (spawnerAllowed)
+        {
+            int randomSpawnPoint = Random.Range(0, spawnPoints.Length);
+            int randomEnemy = Random.Range(0, enemies.Length);
+            Instantiate(enemies[randomEnemy], spawnPoints[randomSpawnPoint].position, Quaternion.identity);
+        }
+    }
+
+}
