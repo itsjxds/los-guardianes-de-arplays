c0df2a0ba7f485350d57bdaa2b45b978628f3927
Jud <judith.aguilar@gracia.lasalle.cat>
2020-03-03 10:31:19 +0100

    hehe oops

--- develop/prototype-1/Los Guardianes de Arlays/Assets/Menus/Scripts/MenuManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class MenuManager
-{
-    public static string backMenu;
-    public static int levelAt = 0;
-}
